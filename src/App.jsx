7a4d101ffca06f9857bf159c606db5f21a7e4a06
SabinaMarium <sabinamarium@gmail.com>
2026-02-06 20:09:50 +0600

    main.jsx edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
